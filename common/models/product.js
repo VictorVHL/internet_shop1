e646364ac1424186bffc45843cbe6e008f6cce25
Victor <vchernyuk987gmail.com>
2019-03-22 17:24:16 +0200

    Have changed method in product.js

--- common/models/product.js
+++ common/models/product.js
@@ -1,18 +1,25 @@
 module.exports = function (Product) {
     Product.observe('before save', async (ctx, next) => {
-        if (ctx.data) {
+        if (ctx.data && ctx.data.price) {
+            ctx.hookState.changedPrice = true
+        }
+    })
+
+    Product.observe('after save', async (ctx, next) => {
+        if(ctx.hookState.changedPrice){
             const CartItem = Product.app.models.CartItem;
             const Cart = Product.app.models.Cart;
-            const productId = ctx.currentInstance.id
+            const productId = ctx.instance.id
             const cartitem = await CartItem.find({ where: { productId } });
-            for(let value of cartitem){
-            let cart = await Cart.findById(value.cartId)
-            cart.totalSum -= value.totalSum;
-            value.totalSum = value.quantity * ctx.data.price;
-            cart.totalSum += value.totalSum;
-            await cart.save();
-            await value.save();    
-            }  
+            for(let i = 0; i < cartitem.length; i++){
+                let value = cartitem[i]
+                let cart = await Cart.findById(value.cartId)
+                cart.totalSum -= value.totalSum;
+                value.totalSum =  value.quantity * ctx.instance.price;
+                cart.totalSum += value.totalSum;
+                await value.save();
+                await cart.save(); 
+            } 
         }
     })
 }
